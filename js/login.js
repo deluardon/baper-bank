2f3fe91a2c479bce6f6de21b521382919f9cb61a
deluardon <160032360+deluardon@users.noreply.github.com>
2024-04-23 00:49:29 +0600

    first commit

---
+++ js/login.js
@@ -0,0 +1,17 @@
+document.getElementById('btn-submit').addEventListener('click', function(){
+    const emailFind = document.getElementById('user-email');
+    const email = emailFind.value;
+    console.log(email)
+
+    const passwordFind = document.getElementById('user-password');
+    const password = passwordFind.value;
+
+
+    if(email==='bapkabeta@gmail.com' && password==='baperbankertaka'){
+        window.location.href = "bank.html";
+        console.log('valid user');
+    }
+    else{
+        console.log('invalid user');
+    }
+})
